bb2845127882a990680e9d6ef955480599c1d289
bitlucky <lokeshtiwari877@gmail.com>
2020-09-11 03:00:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+const [countries , setCountries] = useState(["USA" , "UK" , "India"]);
+
+
+  //Use effect = runs a piece of code
+  // based on a given condition
+
+  useEffect(() =>{
+    //the code inside here will run once
+    //when the component loads and not again 
+
+
+  }, [countries]); 
+
   return (
     <div className="App">
-      <h1> hello react</h1>
+      <div className="app_header">
+        <h1> Lets build a covid 19 tracker </h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {/* loop through all the countries and show a drop down list of the countries  */}
+            {countries.map((country ) => (
+               <MenuItem value={country }> {country} </MenuItem>
+            ))}
+            {/* <MenuItem value="worldwide"> Option 1 </MenuItem>
+            <MenuItem value="worldwide"> Option 2 </MenuItem>
+            <MenuItem value="worldwide"> Option 3 </MenuItem>
+            <MenuItem value="worldwide"> Option 4 </MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
