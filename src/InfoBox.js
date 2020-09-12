163f5e44c39b144b979dcb818a9ca411fc8b0204
bitlucky <lokeshtiwari877@gmail.com>
2020-09-13 03:35:06 +0530

    Check Cards Are Updated

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Card, CardContent , Typography} from "@material-ui/core"
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function InfoBox({title , cases , total  }) {
-    return (
-       <Card className="infoBox"> 
-            <CardContent>
-                {/* Title i.e corona virus cases  */}
-                <Typography className="infoBox_title" color = "textSecondary">
-                    {title}
-                </Typography>
-                
-                {/* +120k Number of cases  */}
-                <h2 className = "infoBox_cases"> {cases} </h2>
-                
-                {/* 1.2 M total  */}
-                <Typography className = "infoBox_total" color = "textSecondary" >
-                    {total} Total 
-                </Typography>
-            </CardContent>
-       </Card>
-    )
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
+
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
